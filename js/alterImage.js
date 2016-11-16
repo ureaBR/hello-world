90841ae6f86c2421a51e384985d28a45a58fad41
ureaBR <ureagames@gmail.com>
2016-11-16 16:29:46 -0200

    Add files via upload

---
+++ js/alterImage.js
@@ -0,0 +1,31 @@
+function alterImage1 (image){
+        document.getElementById("icon1").src = image;
+}
+function alterImage2 (image){
+        document.getElementById("icon2").src = image;
+}
+function alterImage3 (image){
+        document.getElementById("icon3").src = image;
+}
+
+function alterImage4 (image){
+        document.getElementById("imageBack").src = image;
+}
+
+function alterImage5 (image){
+        document.getElementById("icon5").src = image;
+}
+function alterImage6 (image){
+        document.getElementById("icon6").src = image;
+}
+function alterImage7 (image){
+        document.getElementById("icon7").src = image;
+}
+
+function alterImage8 (image){
+        document.getElementById("icon8").src = image;
+}
+
+function alterImage100 (image){
+        document.getElementById("icon100").src = image;
+}
